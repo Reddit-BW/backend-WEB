db389a4058bbd585be638d905f0ab99874cc31a9
Lynda Santiago <lyntechi@gmail.com>
2020-09-21 18:49:14 -0400

    created endpoints for register and login (#3)

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const server = express();
+const helmet = require("helmet");
+const usersRouter = require("../users/users-router");
+const authRouter = require("../auth/auth-router");
+const authenticate = require("../auth/authenticate-middleware");
 
+server.use(helmet());
+server.use(express.json());
 server.get("/", (req, res) => {
   res.status(200).json({ welcome: "to our API" });
 });
+server.use("/api/users", authenticate, usersRouter);
+server.use("/api/auth", authRouter);
 
 module.exports = server;
