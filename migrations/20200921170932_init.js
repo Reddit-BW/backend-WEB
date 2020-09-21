15f077f7c6d35585a1fd0285cfe90309af2fdc71
Lynda Santiago <lyntechi@gmail.com>
2020-09-21 17:12:34 -0400

    created schema tables

---
+++ migrations/20200921170932_init.js
@@ -0,0 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (users) => {
+      users.increments("user_id").unsigned();
+      users.string("username", 255).notNullable().unique();
+      users.string("password", 255).notNullable();
+    })
+    .createTable("user_posts", (posts) => {
+      posts.increments("post_id").unsigned();
+      posts.string("title").notNullable();
+      posts.string("text").notNullable();
+      posts
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("Cascade");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("user_posts");
+};
