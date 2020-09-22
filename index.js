aa334a6a7cb3b77c8d7be38065fe61e36632591c
Lynda Santiago <lyntechi@gmail.com>
2020-09-22 13:07:46 -0400

    ready to deploy (#4)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require("./api/server.js");
-const port = 1111;
+const port = process.env.PORT || 1111;
 
+//heroku will add a PORT variable to the environment (the server the app runs on)
+//we can read it from there using the process.env object
 server.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
