69cfaece0e75c6843c133ec72870ea3075e82866
Lynda Santiago <lyntechi@gmail.com>
2020-09-23 13:23:50 -0400

    testing connection filename

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "../database/init.db3",
+      filename: "./database/init.db3",
     },
     pool: {
       afterCreate: (conn, done) => {
